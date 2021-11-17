a6203188fafb54f2e05f7e1350236b923b5405ed
hugoresende27 <Hugoresende27@gmail.com>
2021-11-17 13:15:42 +0000

    a

--- script.js
+++ script.js
@@ -17,8 +17,14 @@ const randomFuncion = { //OBJETO randomFunction que vai ter chaves lower, upper,
 }
 
 generateEL.addEventListener('click', () => {
-    const comprimento = +compEL.value
-    console.log(comprimento)//CONSOLE LOG DO COMPRIMENTO DA PASS
+    const comprimento = +compEL.value // + para converter para tipo number
+    //console.log(comprimento)//CONSOLE LOG DO COMPRIMENTO DA PASS
+    const temLower = lowerEL.checked    
+    const temUpper = upperEL.checked
+    const temNums = numsEL.checked
+    const temSimb = simbEL.checked  //devolve true se simbEl estiver checked
+    //console.log(temLower,temUpper,temNums,temSimb)
+    resultadoEL.innerText = gerarPassword (temLower,temUpper,temNums,temSimb, comprimento)//FUNÇÃO gerarPass vai receber parametros temLower,temUpper,temNums,temSimb e comprimento
 })
 
 
@@ -37,6 +43,30 @@ function getRandomNum(){
 function getRandomSimb(){
     const simbolos = '!@#$%&*/[]{}.,'   /*SIMBOLOS SÃO UMA STRING */
     return simbolos[Math.floor(Math.random() * simbolos.length)]    //STRING[RANDOM VEZES O COMPRIMENTO DA STRING] VAI ESCOLHER UM INDEX
+}
+
+
+function gerarPassword (lower,upper,number,symbol, comp) {
+    let pass = ''
+    const tiposCont = lower + upper + number + symbol 
+    //console.log(tiposCont) 
+    const tiposArr = [{lower}, {upper}, {number}, {symbol}].filter( item => Object.values(item)[0])//FILTRO PARA RETIRAR DO ARRAY TUDO O Q FOR FALSE
+    //console.log(tiposArr)
+
+    if(tiposCont === 0){
+        return ''
+    }
+
+    for (let i=0; i < comp ; i += tiposCont){
+        tiposArr.forEach(tipo => {
+            //console.log(tipo)
+            const funcName = Object.keys(tipo)[0]
+            console.log(funcName)//vai devolver low upp num sim comp
+            pass += randomFuncion[funcName]()
+        })
+    }
+    const passFinal = pass.slice (0, comp)
+    return passFinal
 }
 
 console.log(getRandomMinusc(),getRandomMaiusc(),getRandomNum(),getRandomSimb())
